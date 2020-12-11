34964079bc17fb94a299882a1ee618fd5276ea18
Simon Longpré-Landry <simonllandry@hotmail.com>
2020-12-11 17:09:20 -0500

    Initial commit

---
+++ src/Application.kt
@@ -0,0 +1,24 @@
+package com.saga
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.auth.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(Authentication) {
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+    }
+}
+
