0876dc67bb83973e17f592051417815e1b9df4b9
Simon Longpré-Landry <simonllandry@hotmail.com>
2020-12-13 17:52:59 -0500

    Gradle stuff and version corrections

--- src/Application.kt
+++ src/Application.kt
@@ -1,19 +1,17 @@
-package com.saga
+package com.saga.authenticator
 
 import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
-import io.ktor.http.*
 import io.ktor.auth.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
-@Suppress("unused") // Referenced in application.conf
+@Suppress("unused")
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
-    install(Authentication) {
-    }
+    install(Authentication) { }
 
     routing {
         get("/") {
